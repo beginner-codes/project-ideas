6832a4dd79c3bcd167fdfad1e46cd08b5c05ce38
Aryan Arora <arynarora232004@gmail.com>
2021-06-20 15:03:15 -0700

    hopefully fixed the issues

--- solutions/pythagorean-theorom-solver.py
+++ solutions/pythagorean-theorom-solver.py
@@ -1,42 +1,29 @@
 from math import sqrt
 
 def pythagorean_theorom_solver(first_num, second_num, side):
-    if side == 'a':
+    if side != 'c':
         try:
             return sqrt(second_num**2 - first_num**2)
         except ValueError:
-            return "b must be smaller than c."
-    if side == 'b':
-        try:
-            return sqrt(second_num**2 - first_num**2)
-        except ValueError:
-            return "a must be smaller than c."
-    if side == 'c':
-        return sqrt(first_num**2 + second_num**2)
+            if side == 'a':
+                return "a must be shorter than c."
+            return "b must be shorter than c."
+    return sqrt(first_num**2 + second_num**2)
     
         
-
-go_on = 'y'
-
-while go_on == 'y':
-    side = input("what side do you want to calculate(a,b,c): ")
-    if side == 'a':
-        side_b = int(input("Enter the first side: "))
-        side_c = int(input("Enter the second side: "))
+def start():
+    go_on = 'y'
+    while go_on == 'y':
+        side = input("what side do you want to calculate(a,b,c): ")
+        side_1 = int(input("Enter the first side: ")):
+        side_2 = int(input("Enter the second side: "))
+        
         print(pythagorean_theorom_solver(side_b, side_c, side))
-
-    if side == 'b':
-        side_a = int(input("Enter the first side: "))
-        side_c = int(input("Enter the second side: "))
-        print(pythagorean_theorom_solver(side_a, side_c, side))
-
-    if side == 'c':
-        side_b = int(input("Enter the first side: "))
-        side_c = int(input("Enter the second side: "))
-        print(pythagorean_theorom_solver(side_b, side_c, side))
-    go_on = input('Press "y" to continue: ')
-    
-print("Thanks for playing.")
+        
+        go_on = input('Press "y" to continue: ')
+    print("Thanks for playing.")
+if __name__ == "__main__":
+    start()
 
 
 
