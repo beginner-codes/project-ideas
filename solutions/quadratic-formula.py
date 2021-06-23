d42ff269ff89cfa8e553c6f94a7151039ddf1e56
MusaMoses <mmusa.akhan@gmail.com>
2021-06-23 02:18:06 +0000

    Lmao took me what 30 mins just to try and push this

--- solutions/quadratic-formula.py
+++ solutions/quadratic-formula.py
@@ -7,7 +7,7 @@ A simple Quadratic Calculator script returning details about a given quadratic e
 """
 
 def discriminant_calculator(a, b, c):
-    return b**2 - 4 * a *c
+    return b**2 - 4 * a * c
 
 
 def round_complex(complex_num):
@@ -25,12 +25,15 @@ def quadratic_formula(a, b, discriminant):
     return x1, x2
 
 
+def format_equation(a, b, c):
+    return f"{a}x² {f'+ {b}' if b > 0 else f'- {abs(b)}'}x {f'+ {c}' if c > 0 else f'- {abs(c)}'}"
+
+
 def quadratic_calculator(a, b, c):
     """
     The actual calculator function that will do the necessary work for a
     correct quadratic expression to work.
     """
-
     discriminant = discriminant_calculator(a, b, c)
     roots = quadratic_formula(a, b, discriminant)
     formatted_equation = format_equation(a, b, c)
@@ -43,9 +46,6 @@ def quadratic_calculator(a, b, c):
         roots: {f'x₁ = {roots[0]} and x₂ = {roots[1]}' if roots[0] != roots[1] else roots[0]}\n
         """
     )
-
-def format_equation(a, b, c):
-    return f"{a}x² {f'+ {b}' if b > 0 else f'- {abs(b)}'}x {f'+ {c}' if c > 0 else f'- {abs(c)}'}"
 
 
 def main():
